3ec0ae4ac021b795260e47be0f7936bbd4438a0d
kalaiselvan23 <kalaiselvan1623@gmail.com>
2024-09-27 21:23:18 +0530

    add app-confg

---
+++ server/src/configs/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+const config = {
+  db: {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DBNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  },
+  oauth: {
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    redirectUri: process.env.OAUTH_REDIRECT_URI,
+  },
+  sheets:{
+    sheeId:process.env.GOOGLE_SHEET_ID,
+  }
+};
+
+export default config;
+
